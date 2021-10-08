930dbb734d214ae657ca69c0d59dccf07b4a7d3c
Геннадій Павлович Коломоєць <kgp@zsea.edu.ua>
2021-10-08 14:48:14 +0300

    Add changes to initial classes for NumFinder

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -1,25 +1,30 @@
 package tudelft.numfinder;
 
+import org.jetbrains.annotations.NotNull;
+
 public class NumFinder {
     private int smallest = Integer.MAX_VALUE;
     private int largest = Integer.MIN_VALUE;
 
     public void find(int[] nums) {
-        for(int n : nums) {
+        if (nums != null) {
+            for (int n : nums) {
 
-            if(n < smallest)
-                smallest = n;
-            else if (n > largest)
-                largest = n;
-
+                if (n < smallest)
+                    smallest = n;
+                /* wrong operator */
+//            else if (n > largest)
+                if (n > largest)
+                    largest = n;
+            }
         }
     }
 
-    public int getSmallest () {
+    public int getSmallest() {
         return smallest;
     }
 
-    public int getLargest () {
+    public int getLargest() {
         return largest;
     }
 }
