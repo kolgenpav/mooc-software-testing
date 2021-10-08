c7b7dfd3b67ae010be9efb4bd13cf88bc897572e
Геннадій Павлович Коломоєць <kgp@zsea.edu.ua>
2021-10-08 14:25:08 +0300

    Add Topic 1 & 2 Quiz Practical exercise #2 solution

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -1,23 +1,36 @@
 package tudelft.caesarshift;
 
 public class CaesarShiftCipher {
-
-    public String CaesarShiftCipher(String message, int shift){
+    /*Баг - имя метода выбрано как для конструктора - меняю*/
+//    public String CaesarShiftCipher(String message, int shift){
+    public String cipher(String message, int shift) {
+        /*Баг - не обрабатывалась null-строка*/
+        if (message == null) {
+            return "invalid";
+        }
+        /*Баг - не обрабатывалась пустая строка*/
+        if ("".equals(message)) {
+            return "invalid";
+        }
         StringBuilder sb = new StringBuilder();
         char currentChar;
         int length = message.length();
 
-        shift = shift%26;
+        shift = shift % 26;
 
-        for(int i = 0; i < length; i++){
+        for (int i = 0; i < length; i++) {
             currentChar = message.charAt(i);
-           
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
+            /*Баг - в выходную строку не должен добавляться исходный несмещенный символ*/
+//            sb.append(currentChar);
+            /*Баг - не учтена обработка пробела, т.к. после пробела все символы печатные, можно применять и для
+             * него тот же алгоритм шифрования*/
+//            if (currentChar > 'z' || currentChar < 'a') {
+            if ((currentChar > 'z' || currentChar < 'a') && ' ' != currentChar) {
                 return "invalid";
             } else if ((char) (currentChar + shift) > 'z') {
                 currentChar = (char) (currentChar - 26);
-            } else if ((char) (currentChar + shift) < 'a'){
+                /*Добавил условие для обработки пробела*/
+            } else if ((char) (currentChar + shift) < 'a' && ' ' != currentChar) {
                 currentChar = (char) (currentChar + 26);
             }
             sb.append((char) (currentChar + shift));
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,35 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.ghappy.GHappy;
+
+/**
+ * Тест реализации шифра Цезаря.
+ * Use-cases:
+ * 1) строка - все маленькие буквы, смещение - меньше 26
+ * 2) строка - все маленькие буквы и пробел, смещение - меньше 26
+ * 3) строка - все маленькие буквы и пробел, смещение 26
+ * 4) строка - все маленькие буквы и пробел, смещение 0
+ * 5) строка - все маленькие буквы и пробел, смещение - больше 26
+ * 6) строка - присутствует большая буква
+ * 7) строка - присутствует запрещенный символ
+ * 8) строка - пустая
+ * 9) строка - null
+ */
 public class CaesarShiftCipherTest {
 
+    @ParameterizedTest(name = "Input string: {0}, Offset: {1}: Expected output string: {2}")
+    @CsvSource({"abcdef, 3, defghi", "abc def, 3, def#ghi", "abc def, 26, abc def", "abc def, 0, abc def",
+            "abc def, 27, bcd!efg"})
+    public void checkEncryption(String input, int offset, String output) {
+        Assertions.assertEquals(output, new CaesarShiftCipher().cipher(input, offset));
+    }
+
+    @ParameterizedTest(name = "Input string: {0}, Offset: {1}: Expected output string: {2}")
+    @CsvSource({"abCdef, 3, invalid", "abc$def, 26, invalid", "'', 27, invalid", ", 0, invalid"})
+    public void checkWrongInput(String input, int offset, String output) {
+        Assertions.assertEquals(output, new CaesarShiftCipher().cipher(input, offset));
+    }
 }
