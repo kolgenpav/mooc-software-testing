afa816b8065866e3f2a82cfd4c1f4a40927ee67f
Геннадій Павлович Коломоєць <kgp@zsea.edu.ua>
2021-10-17 09:18:34 +0300

    Learns Topic 4

--- src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
+++ src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
@@ -1,16 +1,19 @@
 package tudelft.blackjack;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class BlackJackLineCoverageTest {
 
+//    @Disabled
     @Test
     public void bothPlayersGoTooHigh() {
         int result = new BlackJack().play(30, 30);
         Assertions.assertEquals(0, result);
     }
 
+//    @Disabled
     @Test
     public void leftPlayerWins() {
         int result = new BlackJack().play(10, 9);
--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -6,15 +6,33 @@ import org.junit.jupiter.api.Test;
 public class CountLettersTest {
 
     @Test
-    public void multipleMatchingWords() {
+    public void multipleMatchingWordsS() {
         int words = new CountLetters().count("cats|dogs");
         Assertions.assertEquals(2, words);
     }
 
     @Test
-    public void lastWordDoesNotMatch() {
+    public void lastWordDoesNotMatchS() {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
     }
 
+    /*Added 3 tests for 100% branch and condition coverage*/
+    @Test
+    public void multipleMatchingWordsR() {
+        int words = new CountLetters().count("car|door");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void lastWordDoesNotMatchR() {
+        int words = new CountLetters().count("car|dog");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void allWordDoesNotMatch() {
+        int words = new CountLetters().count("cat|dog");
+        Assertions.assertEquals(0, words);
+    }
 }
