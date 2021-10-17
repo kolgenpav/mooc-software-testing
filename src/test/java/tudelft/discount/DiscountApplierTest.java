63d8b5bc811e46cdd4d2fbe2e54c77d651c67607
Геннадій Павлович Коломоєць <kgp@zsea.edu.ua>
2021-10-17 10:14:12 +0300

    Quiz topics 3 and 4 Exercise #2

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,31 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Unit test with Mockito mock for DAO.
+ */
 public class DiscountApplierTest {
 
+    @Test
+    public void testDiscount() {
+        Product businessProduct = new Product("engine", 1003.0, "BUSINESS");
+        Product homeProduct = new Product("TVSe", 890.0, "HOME");
+        List<Product> productList = Arrays.asList(businessProduct, homeProduct);
+        ProductDao dao = Mockito.mock(ProductDao.class);
+        Mockito.when(dao.all()).thenReturn(productList);
+        DiscountApplier applier = new DiscountApplier(dao);
+        applier.setNewPrices();
+        double[] expectedPrices = {1103.30, 801.00};
+        double[] actualPrices = new double[expectedPrices.length];
+        for (int i = 0; i < actualPrices.length; i++) {
+            actualPrices[i] = productList.get(i).getPrice();
+        }
+        Assertions.assertArrayEquals(expectedPrices, actualPrices, 0.00001);
+    }
 }
